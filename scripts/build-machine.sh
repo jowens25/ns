9753bf64c02e4af6625037bf45bb931e23fac6f2
jowens25 <jowens@novuspower.com>
2025-10-15 10:20:53 -0500

    oops

--- scripts/build-machine.sh
+++ scripts/build-machine.sh
@@ -1,2 +1,2 @@
 docker build -t build-env .
-docker run -v /home/jowens/.ssh:/root/.ssh -it build-env /root/Projects/ns/scripts/build-publish-deb.sh
+docker run -v /home/jowens/.ssh:/root/.ssh -it build-env /root/Projects/build-publish-deb.sh
