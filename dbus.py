e7a439d2692b82c6b77655fc6b05dc29d33e999f
jowens25 <jowens@novuspower.com>
2026-02-10 12:05:11 -0600

    added snmp test......

--- dbus.py
+++ dbus.py
@@ -1,13 +1,21 @@
 from dbus_next import BusType
 from dbus_next.aio import MessageBus
+from dbus_next.glib import MessageBus as SyncBus
 
 global AppBus
+global SysBus
 
 async def setup():
     global AppBus
+    global SysBus
     AppBus = await MessageBus(bus_type=BusType.SYSTEM).connect()
+    
+    SysBus = SyncBus(bus_type=BusType.SYSTEM).connect_sync()
 
 async def cleanup():
     global AppBus
+    global SysBus
     AppBus.disconnect()
+    SysBus.disconnect()
+
 
