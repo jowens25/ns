93ceccbe868f1700e8fca77fa44e8e6bc385f587
jowens25 <jowens@novuspower.com>
2026-02-17 15:46:38 -0600

    lib clean up

--- ns2/ns2/dbus.py
+++ ns2/ns2/dbus.py
@@ -3,8 +3,6 @@ import asyncio
 from dbus_next.aio import MessageBus
 from dbus_next.constants import BusType
 
-from ns2.snmp_interface import SnmpInterface
-from ns2.pam_interface import PamInterface
 
 AppBus: MessageBus = None
 
@@ -28,35 +26,6 @@ async def get_dbus():
     
     
     
-
-
-async def service():
-    
-    bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
-    
-    snmpInterface = SnmpInterface('com.novus.ns.snmp')
-    bus.export('/com/novus/ns', snmpInterface)
-    snmpInterface.bus = bus
-
-    pamInterface = PamInterface('com.novus.ns.pam')
-    bus.export('/com/novus/ns', pamInterface)
-    
-    #userInterface = Superuser('com.novus.ns.super')
-    #bus.export('/com/novus/ns', userInterface)
-    
-    #firewallInterface = FirewalldInterface('com.novus.ns.firewall')
-    #bus.export('/com/novus/ns', firewallInterface)
-
-    #socketInterface = SocketInterface('com.novus.ns.socket')
-    #bus.export('/com/novus/ns', socketInterface)
-
-    print("Starting ns service... com.novus.ns")
-    
-    await bus.request_name('com.novus.ns')
-    await asyncio.Event().wait()
-
-    bus.disconnect()
-
 
 
 
--- ns2/ns2/utils.py
+++ ns2/ns2/utils.py
@@ -1,3 +1,2 @@
 from importlib.resources import files
-ASSETS_DIR = files('ns2') / 'assets'
 INTROSPECTION_DIR = files('ns2') / 'introspection'
