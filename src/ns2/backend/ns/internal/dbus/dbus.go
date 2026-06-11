183531f647b3903ee33b892099737aabdb4415fe
jowens25 <jowens@novuspower.com>
2026-06-11 15:15:08 -0500

    removed more commands

--- src/ns2/backend/ns/internal/dbus/dbus.go
+++ src/ns2/backend/ns/internal/dbus/dbus.go
@@ -3,7 +3,6 @@ package dbus
 import (
 	"fmt"
 
-	"github.com/godbus/dbus/v5"
 	"github.com/jowens25/ns/ns/lib"
 	"github.com/spf13/cobra"
 )
@@ -28,42 +27,42 @@ func Export() *cobra.Command {
 	return cmd
 }
 
-func SeeActions() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "actions",
-		Short: "check user actions",
-		RunE: func(cmd *cobra.Command, args []string) error {
+// func SeeActions() *cobra.Command {
+// 	cmd := &cobra.Command{
+// 		Use:   "actions",
+// 		Short: "check user actions",
+// 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			conn, _ := dbus.SystemBus()
+// 			conn, _ := dbus.SystemBus()
 
-			actions, _ := lib.EnumerateActions(dbus.Sender(conn.Names()[0]))
+// 			actions, _ := lib.EnumerateActions(dbus.Sender(conn.Names()[0]))
 
-			for _, a := range actions {
-				fmt.Println(a.Action_id)
-			}
+// 			for _, a := range actions {
+// 				fmt.Println(a.Action_id)
+// 			}
 
-			return nil
-		},
-	}
-	return cmd
-}
+// 			return nil
+// 		},
+// 	}
+// 	return cmd
+// }
 
-func Test() *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "test",
-		Short: "test dbus functions",
-		RunE: func(cmd *cobra.Command, args []string) error {
+// func Test() *cobra.Command {
+// 	cmd := &cobra.Command{
+// 		Use:   "test",
+// 		Short: "test dbus functions",
+// 		RunE: func(cmd *cobra.Command, args []string) error {
 
-			conn, _ := dbus.SystemBus()
+// 			conn, _ := dbus.SystemBus()
 
-			//actions, _ := lib.EnumerateActions(dbus.Sender(conn.Names()[0]))
-			fmt.Println(lib.GetConnectionCredentials(conn))
+// 			//actions, _ := lib.EnumerateActions(dbus.Sender(conn.Names()[0]))
+// 			fmt.Println(lib.GetConnectionCredentials(conn))
 
-			return nil
-		},
-	}
-	return cmd
-}
+// 			return nil
+// 		},
+// 	}
+// 	return cmd
+// }
 
 var Dbus = &cobra.Command{
 	Use:   "dbus",
@@ -72,7 +71,6 @@ var Dbus = &cobra.Command{
 
 func init() {
 	Dbus.AddCommand(Export())
-
-	Dbus.AddCommand(SeeActions())
-	Dbus.AddCommand(Test())
+	//Dbus.AddCommand(SeeActions())
+	//Dbus.AddCommand(Test())
 }
