f5a60b1a5a8e51c17916fe46695f3a082fd693bc
jowens25 <jowens@novuspower.com>
2026-06-29 16:47:36 -0500

    added gpio stuff

--- ftdi/main.c
+++ ftdi/main.c
@@ -526,6 +526,41 @@ int configureFlash(FT_HANDLE *ftHandle)
 int main(int argc, char *argv[])
 {
 
+    const char *chip_path = "/dev/gpiochip0"; // confirm with gpiodetect
+    unsigned int line_offset = 14;            // confirm with gpioinfo
+
+    struct gpiod_chip *chip = gpiod_chip_open(chip_path);
+    if (!chip)
+    {
+        perror("gpiod_chip_open failed");
+        return 1;
+    }
+
+    struct gpiod_line_settings *settings = gpiod_line_settings_new();
+    gpiod_line_settings_set_direction(settings, GPIOD_LINE_DIRECTION_OUTPUT);
+    gpiod_line_settings_set_output_value(settings, GPIOD_LINE_VALUE_INACTIVE);
+
+    struct gpiod_line_config *line_config = gpiod_line_config_new();
+    gpiod_line_config_add_line_settings(line_config, &line_offset, 1, settings);
+
+    struct gpiod_request_config *request_config = gpiod_request_config_new();
+    gpiod_request_config_set_consumer(request_config, "gpio-example");
+
+    struct gpiod_line_request *request =
+        gpiod_chip_request_lines(chip, request_config, line_config);
+
+    if (!request)
+    {
+        perror("gpiod_chip_request_lines failed");
+        gpiod_line_config_free(line_config);
+        gpiod_line_settings_free(settings);
+        gpiod_request_config_free(request_config);
+        gpiod_chip_close(chip);
+        return 1;
+    }
+
+    gpiod_line_request_set_value(request, 14, GPIOD_LINE_VALUE_ACTIVE);
+
     if (argc < 3)
     {
 
@@ -687,6 +722,14 @@ int main(int argc, char *argv[])
     // fclose(f);
     FT4222_UnInitialize(ftHandle);
     FT_Close(ftHandle);
+
+    gpiod_line_request_set_value(request, line_offset, GPIOD_LINE_VALUE_INACTIVE);
+
+    gpiod_line_request_release(request);
+    gpiod_line_config_free(line_config);
+    gpiod_line_settings_free(settings);
+    gpiod_request_config_free(request_config);
+    gpiod_chip_close(chip);
 
     return 0;
 }
